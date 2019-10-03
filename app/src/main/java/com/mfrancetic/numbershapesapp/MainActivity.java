ec334e75b99509a111b00c25fec9b9762cd50b50
Maja Francetic <francetic.maja@gmail.com>
2019-10-03 18:56:31 +0200

    Add an OnClickListener to the checkButton that gets the text from the EditText and clears it

--- app/src/main/java/com/mfrancetic/numbershapesapp/MainActivity.java
+++ app/src/main/java/com/mfrancetic/numbershapesapp/MainActivity.java
@@ -3,12 +3,44 @@ package com.mfrancetic.numbershapesapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText numberEditText;
+    private Button checkButton;
+
+    private int enteredNumber;
+    private String enteredText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        numberEditText = findViewById(R.id.number_edit_text);
+        checkButton = findViewById(R.id.check_button);
+
+        checkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkNumberShape();
+                clearEditText();
+            }
+        });
+
     }
+
+    private void checkNumberShape() {
+        enteredText = numberEditText.getText().toString();
+        enteredNumber = Integer.parseInt(enteredText);
+    }
+
+    private void clearEditText() {
+        numberEditText.setText("");
+    }
+
 }
